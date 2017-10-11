c7e169f414e997629e6603ca7766deb8c4241ce6
CNC3P0 <rspanell@hotmail.com>
2017-10-10 20:27:51 -0700

    Just needs to send from fragment (to mainactivity?) to fragment.

--- app/src/main/java/com/example/robert/tourguide/BlankFragment.java
+++ app/src/main/java/com/example/robert/tourguide/BlankFragment.java
@@ -9,14 +9,21 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.content.Intent;
 
 public class BlankFragment extends Fragment {
 
     View view;
 
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        BlankFragment activity = (BlankFragment) getActivity();
+
+        String blurb;
+        blurb = activity.getIntent().getExtras().getString("message");
 // Inflate the layout for this fragment
         view = inflater.inflate(R.layout.blank_fragment, container, false);
 
--- app/src/main/java/com/example/robert/tourguide/ListFragment.java
+++ app/src/main/java/com/example/robert/tourguide/ListFragment.java
@@ -5,22 +5,44 @@ package com.example.robert.tourguide;
  */
 
 import android.app.Fragment;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ListView;
 
 public class ListFragment extends Fragment {
 
     View view;
+    ListView listView;
+    String[] blurbItems;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 // Inflate the layout for this fragment
         view = inflater.inflate(R.layout.list_fragment, container, false);
+        listView = view.findViewById(R.id.pub_list);
 
+        blurbItems = getResources().getStringArray(R.array.blurbs);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Log.v("potato", "selected item is: "+ blurbItems[i]);
+                handleSelection(i);
+            }
+        });
         return view;
     }
 
+    public void handleSelection(int i) {
+        Intent intent = new Intent(getActivity(), BlankFragment.class);
+        intent.putExtra("message", blurbItems[i]);
+        startActivity(intent);
+    }
 }
